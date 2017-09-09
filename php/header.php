63c04974a294f7e13782cb0149f26686f5a16acf
Alex Beals <alexcbeals@gmail.com>
2017-09-09 01:05:05 +0000

    Switching over to updated favicons

--- php/header.php
+++ php/header.php
@@ -8,26 +8,14 @@
 		?>
 
 		<!-- Favicon -->
-		<link rel="apple-touch-icon" sizes="57x57" href="/assets/favicon/apple-touch-icon-57x57.png">
-		<link rel="apple-touch-icon" sizes="60x60" href="/assets/favicon/apple-touch-icon-60x60.png">
-		<link rel="apple-touch-icon" sizes="72x72" href="/assets/favicon/apple-touch-icon-72x72.png">
-		<link rel="apple-touch-icon" sizes="76x76" href="/assets/favicon/apple-touch-icon-76x76.png">
-		<link rel="apple-touch-icon" sizes="114x114" href="/assets/favicon/apple-touch-icon-114x114.png">
-		<link rel="apple-touch-icon" sizes="120x120" href="/assets/favicon/apple-touch-icon-120x120.png">
-		<link rel="apple-touch-icon" sizes="144x144" href="/assets/favicon/apple-touch-icon-144x144.png">
-		<link rel="apple-touch-icon" sizes="152x152" href="/assets/favicon/apple-touch-icon-152x152.png">
-		<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-touch-icon-180x180.png">
-		<link rel="icon" type="image/png" href="/assets/favicon/favicon-32x32.png" sizes="32x32">
-		<link rel="icon" type="image/png" href="/assets/favicon/favicon-194x194.png" sizes="194x194">
-		<link rel="icon" type="image/png" href="/assets/favicon/favicon-96x96.png" sizes="96x96">
-		<link rel="icon" type="image/png" href="/assets/favicon/android-chrome-192x192.png" sizes="192x192">
-		<link rel="icon" type="image/png" href="/assets/favicon/favicon-16x16.png" sizes="16x16">
-		<link rel="manifest" href="/assets/favicon/manifest.json">
-		<link rel="shortcut icon" href="/assets/favicon/favicon.ico">
-		<meta name="msapplication-TileColor" content="#2b5797">
-		<meta name="msapplication-TileImage" content="/assets/favicon/mstile-144x144.png">
-		<meta name="msapplication-config" content="/assets/favicon/browserconfig.xml">
-		<meta name="theme-color" content="#ffffff">
+		<link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/home-apple-touch-icon.png?v=QEMGLr35xP">
+		<link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/home-favicon-32x32.png?v=QEMGLr35xP">
+		<link rel="icon" type="image/png" sizes="16x16" href="/assets/favicon/home-favicon-16x16.png?v=QEMGLr35xP">
+		<link rel="manifest" href="/assets/favicon/manifest.json?v=QEMGLr35xP">
+		<link rel="mask-icon" href="/assets/favicon/home-safari-pinned-tab.svg?v=QEMGLr35xP" color="#127ec7">
+		<link rel="shortcut icon" href="/assets/favicon/home-favicon.ico?v=QEMGLr35xP">
+		<meta name="msapplication-config" content="/assets/favicon/browserconfig.xml?v=QEMGLr35xP">
+		<meta name="theme-color" content="#127ec7">
 
 		<!-- SEO and Semantic Markup -->
 
